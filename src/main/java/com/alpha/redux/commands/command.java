4bc1d6bf11fdf8c5d0ce8a41b2e867651cdd8263
huysCRACKEDCODERfrfr <diamonddoggie524@gmail.com>
2022-10-29 17:13:28 -0400

    (+) Fancy Raider (#) Item named added to /show

--- src/main/java/com/alpha/redux/commands/command.java
+++ src/main/java/com/alpha/redux/commands/command.java
@@ -632,7 +632,8 @@ public class command implements CommandExecutor {
                         player.sendMessage(ChatColor.RED + "Please wait a little before doing that!");
                     }else{
                         try{
-                            hoverText(ChatColor.translateAlternateColorCodes('&', "&a&lSHOWOFF! " +  rank.getNameColor(player) + player.getDisplayName() + ChatColor.GRAY + " has: " + ChatColor.RED + "Mystic item"), compileListToString(player.getItemInHand().getItemMeta().getLore()));
+                            hoverText(ChatColor.translateAlternateColorCodes('&',
+                                    "&a&lSHOWOFF! " +  rank.getNameColor(player) + player.getDisplayName() + ChatColor.GRAY + " has: " + player.getItemInHand().getItemMeta().getDisplayName()), compileListToString(player.getItemInHand().getItemMeta().getLore()));
                         }catch (Exception e){
                             player.sendMessage(ChatColor.RED + "Please hold a mystic item!");
                         }
